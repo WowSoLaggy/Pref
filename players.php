c234a0f44b07f0e99a9e20c5b8567827f7f228b2
dohxehapo <dohxehapo@gmail.com>
2016-01-25 01:09:35 +0300

    - added the players page

--- players.php
+++ players.php
@@ -6,11 +6,27 @@
 	</head>
 	
 	<body>
-		<h1>Последние игры</h1>
-		Егоров: 129 -113 -52 388 = 352<br>
-		Дегть: -93 5 -200 92 0 = -196<br>
-		Козлов: 11 131 252 -480 -138 = -224<br>
-		Юля: -45 -23 138 = 70<br>
-		15.01.16
+	
+		<h2>Список игроков</h2>
+		<br>
+		
+		<?php
+		
+		// Calculate players and games
+		
+		include('calculations.php');
+		
+		// Output Top-players
+		
+		echo '<table border="1"><td><th></th><th>Имя</th><th>Счёт</th></td>';
+		for ($i = 0; $i < $num_players; $i++)
+			echo "<tr><td width=20 align=center>".($i + 1).
+			"</td><td width=160><img src='images/".$players_sorted[$i]['image'].
+			"' align=absmiddle hspace=10 vspace=10 width=50>".$players_sorted[$i]['name'].
+			"</td><td width=60 align=center>".$players_sorted[$i]['score']."</td></tr>";
+		echo '</table>';
+		
+		?>
+		
 	</body>
 </html>
