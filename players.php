a0bfb4e8cbf14c1f616f49993a8f9c79f5b87a73
dohxehapo <dohxehapo@gmail.com>
2016-01-25 02:11:08 +0300

    - table

--- players.php
+++ players.php
@@ -24,10 +24,10 @@
 			echo "<tr><td width=20 align=center>".($i + 1).
 			"</td><td width=160><img src='images/".$players[$i]['image'].
 			"' align=absmiddle hspace=10 vspace=10 width=50>".$players[$i]['name'].
-			"</td><td width=70 align=center>".$players[$i]['score'].
-			"</td><td width=40 align=center>".$players[$i]['games'].
-			"</td><td width=50 align=center>".(round($players[$i]['wins'] / $players[$i]['games'] * 100)).
-			"%</td><td width=100 align=center>".
+			"</td><td width=80 align=center>".$players[$i]['score'].
+			"</td><td width=50 align=center>".$players[$i]['games'].
+			"</td><td width=60 align=center>".(round($players[$i]['wins'] / $players[$i]['games'] * 100)).
+			"%</td><td width=120 align=center>".
 			$players[$i]['hill_min']."/".(round($players[$i]['hill_avg']))."/".$players[$i]['hill_max'].
 			"</td></tr>";
 		echo '</table>';
