b98640a2256e08f06d8d9321ba4cf2d98f08e747
dohxehapo <dohxehapo@gmail.com>
2016-01-25 01:11:22 +0300

    - players page bugfix

--- players.php
+++ players.php
@@ -18,12 +18,12 @@
 		
 		// Output Top-players
 		
-		echo '<table border="1"><td><th></th><th>Имя</th><th>Счёт</th></td>';
+		echo '<table border="1"><td><th>Имя</th><th>Счёт</th></td>';
 		for ($i = 0; $i < $num_players; $i++)
 			echo "<tr><td width=20 align=center>".($i + 1).
-			"</td><td width=160><img src='images/".$players_sorted[$i]['image'].
-			"' align=absmiddle hspace=10 vspace=10 width=50>".$players_sorted[$i]['name'].
-			"</td><td width=60 align=center>".$players_sorted[$i]['score']."</td></tr>";
+			"</td><td width=160><img src='images/".$players[$i]['image'].
+			"' align=absmiddle hspace=10 vspace=10 width=50>".$players[$i]['name'].
+			"</td><td width=60 align=center>".$players[$i]['score']."</td></tr>";
 		echo '</table>';
 		
 		?>
