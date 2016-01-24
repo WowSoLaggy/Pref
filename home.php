7cc5774c1a292b00030d51419a99b6469c84c527
dohxehapo <dohxehapo@gmail.com>
2016-01-24 23:00:13 +0300

    - mysql test

--- home.php
+++ home.php
@@ -37,7 +37,10 @@
 		
 		$result = mysql_query("SELECT id FROM games_tbl WHERE 1_name=3 OR 2_name=3 OR 3_name=3 OR 4_name=3;", $myConnect);
 		while ($line = mysql_fetch_array($result))
-			echo $line.' ';
+		{
+			foreach ($line as &elem)
+				echo $elem.' ';
+		}
 		mysql_free_result($result);
 		mysql_close($myConnect);
 		?>
