f85423069481ca3b226a730cc49bbdbcf30d7103
dohxehapo <dohxehapo@gmail.com>
2016-01-24 18:29:33 +0300

    - test checkin

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-Hello, pref!
+<html>
+	<head>
+		<title>Клуб анонимных преферансистов</title>
+		<meta charset="utf-8"/>
+	</head>
+	
+	<frameset rows="100, *, 30" border="0" frameborder="0">
+		<frame src="top.php" name="top" scrolling="no" noresize>
+		<frameset cols="190, *">
+			<frame src="menu.php" name="menu" scrolling="no" noresize>
+			<frame src="home.php" name="content" scrolling="no" noresize>
+		</frameset>
+		<frame src="footer.php" name="footer" scrolling="no" noresize>
+	</frameset>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Клуб анонимных преферансистов</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body>
+		Привет, я меню!
+	</body>
+</html>
