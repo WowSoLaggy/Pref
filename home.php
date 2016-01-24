9df38004b26df4675cfa3de921acf7f70998a263
dohxehapo <dohxehapo@gmail.com>
2016-01-24 23:28:03 +0300

    - mysql test

--- home.php
+++ home.php
@@ -9,14 +9,32 @@
 	
 		<h2>Главная страница</h2>
 		<br>
-		<h3>Топ игроков</h3>
-	
-		<table border="1">
-			<tr><td width=20 align=center>1</td><td width=100>Егоров</td><td width=50 align=right>352</td></tr>
-			<tr><td align=center>2</td><td>Юля</td><td align=right>70</td></tr>
-			<tr><td align=center>3</td><td>Дегть</td><td align=right>-196</td></tr>
-			<tr><td align=center>4</td><td>Козлов</td><td align=right>-224</td></tr>
-		</table>
+		
+		<?php
+		$dbHost='localhost';
+		$dbName='pref';
+		$dbUser='root';
+		$dbPass='AfterWorld';
+		$myConnect = mysql_connect($dbHost, $dbUser, $dbPass);
+		mysql_select_db($dbName, $myConnect);
+		
+		$result = mysql_query("SELECT id, name FROM players_tbl");
+		$count = mysql_num_rows($result);
+		for ($i = 0; $i < $count; $i++)
+		{
+			$players[$i]['id'] = mysql_result($result, $i, 'id');
+			$players[$i]['name'] = mysql_result($result, $i, 'name');
+		}
+		
+		echo '<h3>Топ игроков</h3><table border="1">';
+		for ($i = 0; $i < $count; $i++)
+			echo "<tr><td width=20 align=center>$players[$i]['id']</td><td width=100>$players[$i]['name']</td><td width=50 align=right>352</td></tr>";
+		echo '</table>';
+		
+		mysql_free_result($result);
+		mysql_close($myConnect);
+		
+		?>
 		
 		<br>
 		<h3>Общая статистика</h3>
@@ -25,23 +43,6 @@
 			<tr><td width=220 align=left>Всего сыграно игр:</td><td>5</td>
 			<tr><td>Всего участвовало игроков:</td><td>4</td>
 		</table>
-		
-		<?php
-		$dbHost='localhost';
-		$dbName='pref';
-		$dbUser='root';
-		$dbPass='AfterWorld';
-		
-		$myConnect = mysql_connect($dbHost, $dbUser, $dbPass);
-		mysql_select_db($dbName, $myConnect);
-		
-		$result = mysql_query("SELECT id FROM games_tbl WHERE 1_name=3 OR 2_name=3 OR 3_name=3 OR 4_name=3;", $myConnect);
-		$count = mysql_num_rows($result);
-		for ($i = 0; $i < $count; $i++)
-			echo mysql_result($result, $i, 'id');
-		mysql_free_result($result);
-		mysql_close($myConnect);
-		?>
 		
 	</body>
 </html>
