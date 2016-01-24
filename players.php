8e787db34e813e3180e0457fb4840ff003b47f52
dohxehapo <dohxehapo@gmail.com>
2016-01-25 02:09:33 +0300

    - tables

--- players.php
+++ players.php
@@ -19,14 +19,14 @@
 		// Output Top-players
 		
 		echo '<table border="1"><td><th>Имя</th><th>Общий счёт</th><th>Игры</th><th>Победы</th>
-		<th>Гора (мин/ср/макс)</th></td>';
+		<th>Гора<br>(мин/ср/макс)</th></td>';
 		for ($i = 0; $i < $num_players; $i++)
 			echo "<tr><td width=20 align=center>".($i + 1).
 			"</td><td width=160><img src='images/".$players[$i]['image'].
 			"' align=absmiddle hspace=10 vspace=10 width=50>".$players[$i]['name'].
-			"</td><td width=60 align=center>".$players[$i]['score'].
-			"</td><td width=30 align=center>".$players[$i]['games'].
-			"</td><td width=40 align=center>".(round($players[$i]['wins'] / $players[$i]['games'] * 100)).
+			"</td><td width=70 align=center>".$players[$i]['score'].
+			"</td><td width=40 align=center>".$players[$i]['games'].
+			"</td><td width=50 align=center>".(round($players[$i]['wins'] / $players[$i]['games'] * 100)).
 			"%</td><td width=100 align=center>".
 			$players[$i]['hill_min']."/".(round($players[$i]['hill_avg']))."/".$players[$i]['hill_max'].
 			"</td></tr>";
