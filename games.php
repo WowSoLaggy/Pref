44fb4e4dde7a84ccf0f7ebbe8d8f637603defc57
dohxehapo <dohxehapo@gmail.com>
2016-01-25 22:02:52 +0300

    - date sort

--- games.php
+++ games.php
@@ -18,62 +18,69 @@
 		
 		// Output games
 		
+		$games_sorted = $games;
+		function scoreDate($a, $b)
+		{
+			return (-$b['date'] + $a['date']);
+		}
+		usort($games_sorted, "scoreDate");
+		
 		echo '<table border="1"><td><th width=60>Пуля</th><th width=170>Игрок 1</th><th width=170>Игрок 2</th><th width=170>Игрок 3</th><th width=170>Игрок 4</th><th width=150>Дата</th></td>';
 		for ($i = $num_games - 1; $i >= 0; $i--)
 		{
-			echo "<tr valign=center><td width=20 align=center>".($games[$i]['id'] + 1).
-			"</td><td align=center>".$games[$i]['total'].
+			echo "<tr valign=center><td width=20 align=center>".($games_sorted[$i]['id'] + 1).
+			"</td><td align=center>".$games_sorted[$i]['total'].
 			"</td><td";
 			
-			if ($games[$i]['1_score'] > 0)
+			if ($games_sorted[$i]['1_score'] > 0)
 				echo " bgcolor=#009900";
-			else if ($games[$i]['1_score'] < 0)
+			else if ($games_sorted[$i]['1_score'] < 0)
 				echo " bgcolor=#664400";
 			
-			echo "><img src='images/".$players[$games[$i]['1_name']]['image'].
-			"' align=left hspace=10 vspace=10 width=50><p>Счёт: ".$games[$i]['1_score'].
-			"<br>Гора: ".$games[$i]['1_hill'].
-			"<br>Висты: ".$games[$i]['1_money']."</p>".
+			echo "><img src='images/".$players[$games_sorted[$i]['1_name']]['image'].
+			"' align=left hspace=10 vspace=10 width=50><p>Счёт: ".$games_sorted[$i]['1_score'].
+			"<br>Гора: ".$games_sorted[$i]['1_hill'].
+			"<br>Висты: ".$games_sorted[$i]['1_money']."</p>".
 			"</td><td";
 			
-			if ($games[$i]['2_score'] > 0)
+			if ($games_sorted[$i]['2_score'] > 0)
 				echo " bgcolor=#009900";
-			else if ($games[$i]['2_score'] < 0)
+			else if ($games_sorted[$i]['2_score'] < 0)
 				echo " bgcolor=#664400";
 			
-			echo "><img src='images/".$players[$games[$i]['2_name']]['image'].
-			"' align=left hspace=10 vspace=10 width=50><p>Счёт: ".$games[$i]['2_score'].
-			"<br>Гора: ".$games[$i]['2_hill'].
-			"<br>Висты: ".$games[$i]['2_money']."</p>".
+			echo "><img src='images/".$players[$games_sorted[$i]['2_name']]['image'].
+			"' align=left hspace=10 vspace=10 width=50><p>Счёт: ".$games_sorted[$i]['2_score'].
+			"<br>Гора: ".$games_sorted[$i]['2_hill'].
+			"<br>Висты: ".$games_sorted[$i]['2_money']."</p>".
 			"</td><td";
 			
-			if ($games[$i]['3_score'] > 0)
+			if ($games_sorted[$i]['3_score'] > 0)
 				echo " bgcolor=#009900";
-			else if ($games[$i]['3_score'] < 0)
+			else if ($games_sorted[$i]['3_score'] < 0)
 				echo " bgcolor=#664400";
 			
-			echo "><img src='images/".$players[$games[$i]['3_name']]['image'].
-			"' align=left hspace=10 vspace=10 width=50><p>Счёт: ".$games[$i]['3_score'].
-			"<br>Гора: ".$games[$i]['3_hill'].
-			"<br>Висты: ".$games[$i]['3_money']."</p>".
+			echo "><img src='images/".$players[$games_sorted[$i]['3_name']]['image'].
+			"' align=left hspace=10 vspace=10 width=50><p>Счёт: ".$games_sorted[$i]['3_score'].
+			"<br>Гора: ".$games_sorted[$i]['3_hill'].
+			"<br>Висты: ".$games_sorted[$i]['3_money']."</p>".
 			"</td><td";
 			
-			if ($games[$i]['num_players'] == 4)
+			if ($games_sorted[$i]['num_players'] == 4)
 			{
-				if ($games[$i]['4_score'] > 0)
+				if ($games_sorted[$i]['4_score'] > 0)
 					echo " bgcolor=#009900";
-				else if ($games[$i]['4_score'] < 0)
+				else if ($games_sorted[$i]['4_score'] < 0)
 					echo " bgcolor=#664400";
 			
-				echo "><img src='images/".$players[$games[$i]['4_name']]['image'].
-				"' align=left hspace=10 vspace=10 width=50><p>Счёт: ".$games[$i]['4_score'].
-				"<br>Гора: ".$games[$i]['4_hill'].
-				"<br>Висты: ".$games[$i]['4_money']."</p>";
+				echo "><img src='images/".$players[$games_sorted[$i]['4_name']]['image'].
+				"' align=left hspace=10 vspace=10 width=50><p>Счёт: ".$games_sorted[$i]['4_score'].
+				"<br>Гора: ".$games_sorted[$i]['4_hill'].
+				"<br>Висты: ".$games_sorted[$i]['4_money']."</p>";
 			}
 			else
 				echo ">";
 			
-			echo "</td><td align=center>".$games[$i]['date'].
+			echo "</td><td align=center>".$games_sorted[$i]['date'].
 			"</td></tr>";
 		}
 		echo '</table>';
