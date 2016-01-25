04a887fbc261759a2c7d5d207079b55528b3bfe0
dohxehapo <dohxehapo@gmail.com>
2016-01-25 14:23:56 +0300

    - games page

--- games.php
+++ games.php
@@ -18,18 +18,18 @@
 		
 		// Output games
 		
-		echo '<table border="1"><td><th width=80>Игрок 1</th><th width=160>Игрок 2</th><th width=160>Игрок 3</th><th width=160>Игрок 4</th><th width=60>Пуля</th></td>';
+		echo '<table border="1"><td><th width=60>Пуля</th><th width=70>Игрок 1</th><th width=70>Игрок 2</th><th width=70>Игрок 3</th><th width=70>Игрок 4</th></td>';
 		for ($i = $num_games - 1; $i >= 0; $i--)
 		{
 			echo "<tr><td width=20 align=center>".($games[$i]['id'] + 1).
-			"</td><td><img src='images/".$players[$games[$i]['1_name']]['image']."' align=absmiddle hspace=10 vspace=10 width=50>".
-			"</td><td><img src='images/".$players[$games[$i]['2_name']]['image']."' align=absmiddle hspace=10 vspace=10 width=50>".
-			"</td><td><img src='images/".$players[$games[$i]['3_name']]['image']."' align=absmiddle hspace=10 vspace=10 width=50>".
+			"</td><td align=center>".$games[$i]['total'].
+			"</td><td><img src='images/".$players[$games[$i]['1_name']]['image']."' align=absmiddle hspace=10 vspace=10 width=50>Счёт: 60<br>Гора: 20<br>Висты: 100".
+			"</td><td><img src='images/".$players[$games[$i]['2_name']]['image']."' align=absmiddle hspace=10 vspace=10 width=50>Счёт: 60<br>Гора: 20<br>Висты: 100".
+			"</td><td><img src='images/".$players[$games[$i]['3_name']]['image']."' align=absmiddle hspace=10 vspace=10 width=50>Счёт: 60<br>Гора: 20<br>Висты: 100".
 			"</td><td>";
 			if ($games[$i]['num_players'] == 4)
-				echo "<img src='images/".$players[$games[$i]['4_name']]['image']."' align=absmiddle hspace=10 vspace=10 width=50>";
-			echo "</td><td>".$games[$i]['total'].
-			"</td></tr>";
+				echo "<img src='images/".$players[$games[$i]['4_name']]['image']."' align=absmiddle hspace=10 vspace=10 width=50>Счёт: 60<br>Гора: 20<br>Висты: 100";
+			echo "</td></tr>";
 		}
 		echo '</table>';
 		
