3557532788ef02b574d57273d8daa63195ebd18d
dohxehapo <dohxehapo@gmail.com>
2016-01-28 01:43:14 +0300

    - test

--- games.php
+++ games.php
@@ -16,6 +16,7 @@
 			echo '<table border="1"><td><th width=60>Пуля</th><th width=170>Игрок 1</th><th width=170>Игрок 2</th><th width=170>Игрок 3</th><th width=170>Игрок 4</th><th width=100>Дата</th></td>';
 			for ($i = $startIndex; $i <= $endIndex; $i++)
 			{
+				echo "<a class='anchor' name='anchor1_game_".($num_games - $i)."'></a>";
 				echo "<tr valign=center><td width=30 align=center><a class='anchor' name='anchor_game_".($num_games - $i)."'>".($num_games - $i).
 				"</a></td><td align=center>".$games[$i]['total'].
 				"</td><td";
