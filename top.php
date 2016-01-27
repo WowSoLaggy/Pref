cb01d1e3c79750c545a6660998f96fae59959cf4
dohxehapo <dohxehapo@gmail.com>
2016-01-28 01:00:33 +0300

    - beautified title

--- top.php
+++ top.php
@@ -7,6 +7,6 @@
 	
 	<body class="dark">
 		<div id='logo_block'><img id='logo' src="images/kand.png"></div>
-		<div id='logo_title'><h1 align=center>Клуб "Паровоз Козлова"</h1></div>
+		<div id='logo_title'><img src="images/title.png"></div>
 	</body>
 </html>
