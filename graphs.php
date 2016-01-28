3a03777f956f8240179b575f4d533018525c4358
dohxehapo <dohxehapo@gmail.com>
2016-01-28 11:41:53 +0300

    - added graphs page

---
+++ graphs.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Клуб "Паровоз Козлова"</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body class="light">
+	
+		<h2>Весёлые графики</h2>
+		
+		Если что, тут пока ничего нет.
+		
+	</body>
+</html>
