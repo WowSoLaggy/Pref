3176b01bcb045b171d54e33ac49d1ba855c2c5fa
dohxehapo <dohxehapo@gmail.com>
2016-01-28 11:48:07 +0300

    - encoding bugfix

--- graphs.php
+++ graphs.php
@@ -1,15 +1,15 @@
 <html>
 	<head>
-		<title>Клуб "Паровоз Козлова"</title>
+		<title>РљР»СѓР± "РџР°СЂРѕРІРѕР· РљРѕР·Р»РѕРІР°"</title>
 		<meta charset="utf-8"/>
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	
 	<body class="light">
 	
-		<h2>Весёлые графики</h2>
+		<h2>Р’РµСЃС‘Р»С‹Рµ РіСЂР°С„РёРєРё</h2>
 		
-		Если что, тут пока ничего нет.
+		РўСѓС‚, РєСЃС‚Р°С‚Рё, РїРѕРєР° РЅРёС‡РµРіРѕ РЅРµС‚.
 		
 	</body>
 </html>
