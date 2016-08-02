bb329b8910c81e928fee38d92880ba6824227139
dohxehapo <dohxehapo@gmail.com>
2016-08-02 22:48:17 +0300

    - added empty seasons page

---
+++ seasons.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body class="light">
+	
+		<h2>Сезоны</h2>
+		<br>
+		
+		<br>
+		Очень (вообще, не очень) скоро (не скоро) тут что-нибудь появится.
+		<br>
+		
+	</body>
+</html>
