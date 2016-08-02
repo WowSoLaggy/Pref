341140c7ef8c6ec48a07497036ee7020bb0cb4e9
dohxehapo <dohxehapo@gmail.com>
2016-08-02 22:50:48 +0300

    - seasons header bugfix

--- seasons.php
+++ seasons.php
@@ -1,4 +1,8 @@
-<link rel="stylesheet" type="text/css" href="style.css">
+<html>
+	<head>
+		<title>Клуб "Паровоз Козлова"</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	
 	<body class="light">
