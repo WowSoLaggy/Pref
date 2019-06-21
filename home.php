9f1a1e151bf34e170259e0e1026f5ebf37e8053f
WowSoLaggy <dohxehapo@gmail.com>
2019-06-21 23:52:35 +0300

    - Migrated 'home.php' to classes

--- home.php
+++ home.php
@@ -20,7 +20,7 @@
 		$players_sorted = $players;
 		function scoreSort($a, $b)
 		{
-			return ($b['score'] - $a[score]);
+			return ($b->score - $a->score);
 		}
 		usort($players_sorted, "scoreSort");
 		
@@ -28,10 +28,10 @@
 		for ($i = 0; $i < $num_players; $i++)
 		{
 			echo "<tr><td width=20 align=center>".($i + 1).
-			"</td><td width=160><img src='images/".$players_sorted[$i]['image'].
-			"' align=absmiddle hspace=10 vspace=10 width=50>".$players_sorted[$i]['name'].
-			"</td><td width=50 align=center>".$players_sorted[$i]['games'].
-			"</td><td width=100 align=center>".round($players_sorted[$i]['score'])."</td></tr>";
+			"</td><td width=160><img src='images/".$players_sorted[$i]->image.
+			"' align=absmiddle hspace=10 vspace=10 width=50>".$players_sorted[$i]->name.
+			"</td><td width=50 align=center>".$players_sorted[$i]->games.
+			"</td><td width=100 align=center>".round($players_sorted[$i]->score)."</td></tr>";
 		}
 		echo '</table></div>';
 		
@@ -42,13 +42,13 @@
 			<tr><td>Общая длина всех пуль:</td><td>'.$total.'</td></tr>
 			<tr><td>Всего участвовало игроков:</td><td>'.$num_players.'</td></tr>
 			<tr><td>Наибольшая победа:</td><td><a class="anchor_link" href="games.php#anchor_game_'.
-			($num_games - $max_win_ind).'">'.round($max_win).' ('.$players[$max_win_player]['name'].')</a></td></tr>
+			($num_games - $max_win_ind).'">'.round($max_win).' ('.$players[$max_win_player]->name.')</a></td></tr>
 			<tr><td>Наибольший проигрыш:</td><td><a class="anchor_link" href="games.php#anchor_game_'.
-			($num_games - $max_loss_ind).'">'.round($max_loss).' ('.$players[$max_loss_player]['name'].')</a></td></tr>
+			($num_games - $max_loss_ind).'">'.round($max_loss).' ('.$players[$max_loss_player]->name.')</a></td></tr>
 			<tr><td>Наибольшая гора:</td><td><a class="anchor_link" href="games.php#anchor_game_'.
-			($num_games - $max_hill_ind).'">'.$max_hill.' ('.$players[$max_hill_player]['name'].')</a></td></tr>
+			($num_games - $max_hill_ind).'">'.$max_hill.' ('.$players[$max_hill_player]->name.')</a></td></tr>
 			<tr><td>Наименьшая гора:</td><td><a class="anchor_link" href="games.php#anchor_game_'.
-			($num_games - $min_hill_ind).'">'.$min_hill.' ('.$players[$min_hill_player]['name'].')</a></td></tr>
+			($num_games - $min_hill_ind).'">'.$min_hill.' ('.$players[$min_hill_player]->name.')</a></td></tr>
 			</table></div>';
 		
 		?>
