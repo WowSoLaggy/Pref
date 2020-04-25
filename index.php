eefdefc2920c8cb2078db222ab3b7eca0fbe8d30
Anton Egorov <dohxehapo@gmail.com>
2020-04-25 15:41:48 +0300

    Renamed 'top.php' -> 'header.php'

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	</head>
 	
 	<frameset rows="100, *, 30" border="0" frameborder="0">
-		<frame src="top.php" name="top" scrolling="no" noresize>
+		<frame src="header.php" name="top" scrolling="no" noresize>
 		<frameset cols="190, *">
 			<frame src="menu.php" name="menu" scrolling="no" noresize>
 			<frame src="home.php" name="content" scrolling="auto" noresize>
