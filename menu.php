ddb1c2a6abc7396b4ef45e31fc08828111ef87dd
Anton Egorov <dohxehapo@gmail.com>
2020-04-28 23:26:53 +0300

    Draw 'menu.php' as a single string

--- menu.php
+++ menu.php
@@ -1,17 +1,30 @@
-<html>
-	<head>
-		<title>Клуб "Паровоз Козлова"</title>
-		<meta charset="utf-8"/>
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-	
-	<body class="menu">
-	
-		<div><img style="margin: 10px 0px 15px 0px;" src="images/menu.png"></div>
-		<div><a href="home.php" target="content"><img class="menu_image" src="images/btn_menu_main.png"></a></div>
-		<div><a href="players.php" target="content"><img class="menu_image" src="images/btn_menu_players.png"></a></div>
-		<div><a href="games.php" target="content"><img class="menu_image" src="images/btn_menu_games.png"></a></div>
-		<div><a href="seasons.php" target="content"><img class="menu_image" src="images/btn_menu_seasons.png"></a></div>
+<?php
 
-	</body>
-</html>
+
+function get_page_str()
+{
+  include('utils.php');
+  
+  $str = get_header_str();
+  $str .= '
+    <body class="menu">
+      <div><img style="margin: 10px 0px 15px 0px;" src="images/menu.png"></div>
+      <div><a href="home.php" target="content"><img class="menu_image" src="images/btn_menu_main.png"></a></div>
+      <div><a href="players.php" target="content"><img class="menu_image" src="images/btn_menu_players.png"></a></div>
+      <div><a href="games.php" target="content"><img class="menu_image" src="images/btn_menu_games.png"></a></div>
+      <div><a href="seasons.php" target="content"><img class="menu_image" src="images/btn_menu_seasons.png"></a></div>
+    </body></html>';
+
+	return $str;
+} // get_page_str()
+
+
+//
+// MAIN
+//
+
+
+$str = get_page_str();
+echo($str);
+
+?>
