9f6799d57868855af6c209906c779f65b765ea50
Anton Egorov <dohxehapo@gmail.com>
2020-04-28 23:15:01 +0300

    Draw 'header.php' as a single string

--- header.php
+++ header.php
@@ -1,12 +1,26 @@
-<html>
-	<head>
-		<title>Клуб "Паровоз Козлова"</title>
-		<meta charset="utf-8"/>
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-	
-	<body class="dark">
-		<div id='logo_block'><img id='logo' src="images/kand.png"></div>
-		<div id='logo_title'><img src="images/title.png"></div>
-	</body>
-</html>
+<?php
+
+
+function get_page_str()
+{
+  include('utils.php');
+  
+  $str = get_header_str();
+  $str .= '
+    <body class="dark">
+      <div id="logo_block"><img id="logo" src="images/kand.png"></div>
+      <div id="logo_title"><img src="images/title.png"></div>
+    </body></html>';
+	return $str;
+} // get_page_str()
+
+
+//
+// MAIN
+//
+
+
+$str = get_page_str();
+echo($str);
+
+?>
