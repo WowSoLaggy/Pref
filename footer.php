1da6949a8a98eaad3a757d8af05c1c08f046fadf
Anton Egorov <dohxehapo@gmail.com>
2020-04-28 22:36:36 +0300

    Draw 'footer.php' as a single string

--- footer.php
+++ footer.php
@@ -1,11 +1,25 @@
-<html>
-	<head>
-		<title>Клуб "Паровоз Козлова"</title>
-		<meta charset="utf-8"/>
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-	
-	<body class="dark">
-		<div align=right>WowSoLaggy, 2016-2020 (c)</div>
-	</body>
-</html>
+<?php
+
+
+function get_page_str()
+{
+  include('utils.php');
+  
+  $str = get_header_str();
+  $str .= '
+    <body class="dark">
+      <div align=right>WowSoLaggy, 2016-2020 (c)</div>
+    </body></html>';
+	return $str;
+} // get_page_str()
+
+
+//
+// MAIN
+//
+
+
+$str = get_page_str();
+echo($str);
+
+?>
