7da5d33a23ce5ff5b1ee43c9224cb424d554d90a
Anton Egorov <dohxehapo@gmail.com>
2020-04-28 23:24:50 +0300

    Draw 'index.php' as a single string

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
-<html>
-	<head>
-		<title>Клуб "Паровоз Козлова"</title>
-		<meta charset="utf-8"/>
-		<link rel="icon" href="favicon.ico" type="image/ico"/>
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-	</head>
-	
-	<frameset rows="100, *, 30" border="0" frameborder="0">
-		<frame src="header.php" name="top" scrolling="no" noresize>
-		<frameset cols="190, *">
-			<frame src="menu.php" name="menu" scrolling="no" noresize>
-			<frame src="home.php" name="content" scrolling="auto" noresize>
-		</frameset>
-		<frame src="footer.php" name="footer" scrolling="no" noresize>
-	</frameset>
+<?php
 
-</html>
+
+function get_page_str()
+{
+  include('utils.php');
+  
+  $str = get_header_str();
+  $str .= '
+    <frameset rows="100, *, 30" border="0" frameborder="0">
+      <frame src="header.php" name="top" scrolling="no" noresize>
+      <frameset cols="190, *">
+        <frame src="menu.php" name="menu" scrolling="no" noresize>
+        <frame src="home.php" name="content" scrolling="auto" noresize>
+      </frameset>
+      <frame src="footer.php" name="footer" scrolling="no" noresize>
+    </frameset></html>';
+	return $str;
+} // get_page_str()
+
+
+//
+// MAIN
+//
+
+
+$str = get_page_str();
+echo($str);
+
+?>
