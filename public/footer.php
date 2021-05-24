73e9ac149916a65a736a6ffea323adceeba9b18b
Anton Egorov <dohxehapo@gmail.com>
2021-05-24 21:16:57 +0300

    Added auto-update of the year in footer

--- public/footer.php
+++ public/footer.php
@@ -8,7 +8,7 @@ function get_page_str()
   $str = get_header_str();
   $str .= '
     <body class="dark">
-      <div align=right>WowSoLaggy, 2016-2020 (c)</div>
+      <div align=right>WowSoLaggy, 2016-'.date("Y").' (c)</div>
     </body></html>';
 	return $str;
 } // get_page_str()
